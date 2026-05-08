f2099d5416d66c648bdd48543a7726804034137e
Himtech <tech.himtech@gmail.com>
2026-05-08 14:14:37 +0530

    feat: add Sanity CLI configuration for project setup

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'v9lzjatz',
+    dataset: 'production',
+  },
+  studioHost: 'myschool',
+})
